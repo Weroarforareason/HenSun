0dc85f344cd69c7eec4f992d054ff1c274816ae1
Weroarforareason <Weroarforareason@Gmail.com>
2025-10-07 18:08:19 -0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Rocket Launch Today.");
+        for (int i=100;i>0;i--){
+            System.out.println(i+"...");
+        }
+        System.out.println("Blastoff!");
+    }
+}
