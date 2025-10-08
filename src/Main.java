7964ad82d3f9a780dafcb69ccc3fb24bc55c470e
Weroarforareason <Weroarforareason@Gmail.com>
2025-10-07 18:16:38 -0700

    Uh, test for commits

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Rocket Launch Today.");
-        for (int i=100;i>0;i--){
-            System.out.println(i+"...");
+        for (int i=100;i>0;i--) {
+            for (int j=100;j>0;j--){
+                System.out.println(j + "...");
+            }
         }
         System.out.println("Blastoff!");
     }
