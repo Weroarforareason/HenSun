68dce25e81632ff6fbff11cc39b45b80b33ee4da
Weroarforareason <Weroarforareason@Gmail.com>
2025-10-14 18:13:38 -0700

    Weekly update

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,6 @@
-public class Main {
+import java.util.Scanner;
+public class Main{
     public static void main(String[] args) {
-        System.out.println("Rocket Launch Today.");
-        for (int i=100;i>0;i--) {
-            for (int j=100;j>0;j--){
-                System.out.println(j + "...");
-            }
-        }
-        System.out.println("Blastoff!");
-        System.out.println("Unfortunately, the rocket crashed(really, this is just a troupe for me to mess around with commits.");
+
     }
 }
--- src/MeVars.java
+++ src/MeVars.java
@@ -1,19 +1,14 @@
 import java.util.Scanner;
-public class MeVars {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("enter your name");
-        String myName = input.nextLine();
-        System.out.println("enter your age");
-        String myAge = input.nextLine();
-        System.out.println("enter your house address");
-        String myHouseAddress = input.nextLine();
-        System.out.println("enter your current school/job");
-        String mySchoolOrJob = input.nextLine();
-        System.out.println("enter your nickname");
-        String myNickname = input.nextLine();
-        System.out.println("enter your earnings in dollars, if none, enter 0");
-        String mySalary = input.nextLine();
-        System.out.println("Hello, my name is "+myName+". I am "+myAge+" years old. I live at "+myHouseAddress+". My nickname is "+myNickname+" I go to school or work at "+mySchoolOrJob+", Where I earn "+mySalary+" dollars.");
+public class MeVars{
+    Scanner scanner=new Scanner(System.in);
+            String name;
+    int age;
+    String hobby;
+    public int getAge(){
+        System.out.println("Input age.");
+        return scanner.nextInt();
+    }
+    public MeVars(){
+        this.age=getAge();
     }
 }
