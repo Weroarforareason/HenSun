7b526bfb9d9c90bde60dcb04fcf79b405eafce3c
Weroarforareason <Weroarforareason@Gmail.com>
2025-10-14 18:24:01 -0700

    In class changes

--- src/MeVars.java
+++ src/MeVars.java
@@ -8,7 +8,17 @@ public class MeVars{
         System.out.println("Input age.");
         return scanner.nextInt();
     }
+
+    public String getHobby() {
+        return scanner.nextLine();
+    }
+    public String getName(){
+        return scanner.nextLine();
+    }
+
     public MeVars(){
         this.age=getAge();
+        this.hobby=getHobby();
+        this.name=getName();
     }
 }
